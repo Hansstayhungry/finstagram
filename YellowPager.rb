ee5cd0acaf9ab0b48e52106caf64558690fc50c4
Hansstayhungry <hansmikaa@live.cn>
2022-02-06 22:39:56 +0000

    W3D2

---
+++ YellowPager.rb
@@ -0,0 +1,17 @@
+=begin
+- Write a method that accepts a 10 character string of letters (both uppercase and lowercase)
+- if not 10 characters in length, return false
+- else:
+  - define empty string variable (phone_number)
+  - convert string into an array
+  - loop through array
+  - compare each letter in the array to telephone mappings and concat or add to variable (phone_number)
+  - return the variable (phone_number)
+
+* You may need to ensure the letter casing of the input string *
+=end
+
+
+def yellow_page(word)
+        phone_number = ""
+end
